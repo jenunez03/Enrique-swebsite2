ae9a6fdc8f57e58fb0e84f907690f8920981d200
jenunez03 <151690498+jenunez03@users.noreply.github.com>
2024-03-19 12:10:15 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+<script>
+    const video = document.getElementById('video');
+    
+    video.loop = true;
+
+    function setLooping(enableLoop) {
+        video.loop = enableLoop;
+    }
+
+    setLooping(true); 
+   
+   
+</script>
